1184199445326f84dc5590ec8106cb0f8255470a
Mark Chen <markchen555@gmail.com>
2018-01-16 23:43:24 -0800

    Doc: modify action and connect to click event in book-list.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,12 @@
 export function selectBook(book) {
-  console.log('A book has been selected: ', book.title);
+  // Testing
+  // console.log('A book has been selected: ', book.title);
+
+  // SelectBook is an ActionCreator, it needs to return an action, 
+  // an object with a type property and sometimes contains payload.
+  // type is alway Uppercase.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 };
